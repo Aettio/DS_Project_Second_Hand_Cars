595c99ed22b1f98681b7684f2295d817545c2364
Roman <61010440+Aettio@users.noreply.github.com>
2021-03-04 14:30:56 +0200

    Update Code_Visuals.py

--- Code_Visuals.py
+++ Code_Visuals.py
@@ -15,15 +15,18 @@ sns.heatmap(clean_train.corr(), annot = True, cmap= 'viridis', annot_kws={"size"
 
 # Проверка корреляции 1
 
-graph_km = sns.scatterplot(data = df, x = 'current price',y = 'km')
+graph_km = sns.scatterplot(data = df, x = 'current price',y = 'km', hue='current price',
+                                    legend=False,palette='viridis')
 graph_km.set(xlabel='Current price', ylabel='Km')
 
 # Проверка корреляции 2
 
-graph_on_road_now = sns.scatterplot(data = df, x = 'current price',y = 'on road now')
+graph_on_road_now = sns.scatterplot(data = df, x = 'current price',y = 'on road now', hue='current price',
+                                    legend=False,palette='viridis')
 graph_on_road_now.set(xlabel='Current price', ylabel='On road now')
 
 # Проверка корреляции 3
 
-graph_on_road_old = sns.scatterplot(data = df, x = 'current price',y = 'on road old')
+graph_on_road_old = sns.scatterplot(data = df, x = 'current price',y = 'on road old', hue='current price',
+                                    legend=False,palette='viridis')
 graph_on_road_old.set(xlabel='Current price', ylabel='On road old')
